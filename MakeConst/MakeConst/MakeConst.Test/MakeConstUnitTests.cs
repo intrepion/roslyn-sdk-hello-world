a0fa471f4eaaf0954036752ba8ccb29fbc686a5c
Oliver Forral <intrepion@gmail.com>
2021-02-25 23:27:13 -0800

    Create tests for valid declarations

--- MakeConst/MakeConst/MakeConst.Test/MakeConstUnitTests.cs
+++ MakeConst/MakeConst/MakeConst.Test/MakeConstUnitTests.cs
@@ -9,9 +9,90 @@ namespace MakeConst.Test
     [TestClass]
     public class MakeConstUnitTest
     {
+        private const string VariableAssigned = @"
+using System;
+
+namespace MakeConstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine(i++);
+        }
+    }
+}";
+
+        private const string AlreadyConst = @"
+using System;
+
+namespace MakeConstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string NoInitializer = @"
+using System;
+
+namespace MakeConstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 0;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string InitializerNotConstant = @"
+using System;
+
+namespace MakeConstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = DateTime.Now.DayOfYear;
+            Console.WriteLine(i);
+        }
+    }
+}";
+
+        private const string MultipleInitializers = @"
+using System;
+
+namespace MakeConstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = DateTime.Now.DayOfYear;
+            Console.WriteLine(i, j);
+        }
+    }
+}";
+
         //No diagnostics expected to show up
         [DataTestMethod]
-        [DataRow("")]
+        [DataRow(""),
+            DataRow(VariableAssigned),
+            DataRow(AlreadyConst),
+            DataRow(NoInitializer),
+            DataRow(InitializerNotConstant),
+            DataRow(MultipleInitializers)]
         public async Task WhenTestCodeIsValidNoDiagnosticIsTriggered(string testCode)
         {
             await VerifyCS.VerifyAnalyzerAsync(testCode);
